405ea58e97aac717b82d98df81bb38128585759c
Steven Simon <stevierichard1@gmail.com>
2021-03-29 02:39:59 -0700

    Updates

---
+++ precache-manifest.1ffa759313b339ad2d112177d2280193.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581767212c0a2c3aa149f9c79163577b",
+    "url": "/react_portifolio_app/index.html"
+  },
+  {
+    "revision": "5eb8a605b76054893126",
+    "url": "/react_portifolio_app/static/css/main.7af57b61.chunk.css"
+  },
+  {
+    "revision": "54ad027b79ac943633ca",
+    "url": "/react_portifolio_app/static/js/2.4a702829.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/react_portifolio_app/static/js/2.4a702829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8a605b76054893126",
+    "url": "/react_portifolio_app/static/js/main.c46b37a1.chunk.js"
+  },
+  {
+    "revision": "1121d148152ba364cd26",
+    "url": "/react_portifolio_app/static/js/runtime-main.bbb1269f.js"
+  },
+  {
+    "revision": "6c559d12efea4c091b425e2ae28281a1",
+    "url": "/react_portifolio_app/static/media/Steven Richard Simon resume.6c559d12.pdf"
+  },
+  {
+    "revision": "36fe5feaeff842ff62eda2fca35cf5b4",
+    "url": "/react_portifolio_app/static/media/StockTracker.36fe5fea.jpg"
+  },
+  {
+    "revision": "856d2151378da6680f0d9c18b103678c",
+    "url": "/react_portifolio_app/static/media/calender.856d2151.jpg"
+  },
+  {
+    "revision": "0bf442e58abf94f6a4cc0541e8153286",
+    "url": "/react_portifolio_app/static/media/password_generator.0bf442e5.jpg"
+  },
+  {
+    "revision": "4ad6f4e25f4986483daf1ee8e9c06937",
+    "url": "/react_portifolio_app/static/media/readme.4ad6f4e2.jpg"
+  },
+  {
+    "revision": "2135b047efc2e0f941466909ebd91b59",
+    "url": "/react_portifolio_app/static/media/steve.2135b047.jpeg"
+  },
+  {
+    "revision": "f8eeaf8792143359ab49bee9eda7af37",
+    "url": "/react_portifolio_app/static/media/weather.f8eeaf87.jpg"
+  }
+]);
